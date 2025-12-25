092c05f355f3e2772d5bce2dee139dd0bdb6b4e3
Shashank2125 <138694950+Shashank2125@users.noreply.github.com>
2025-12-25 21:53:10 +0530

    Setup and UI for testing lobby

---
+++ Assets/Scripts/UI/TestingLobbyUI.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestingLobbyUI : MonoBehaviour
+{
+    [SerializeField] private Button createGameButton;
+    [SerializeField] private Button joinGameButton;
+}
